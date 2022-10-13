b1ce983e2d5b72759778f68ef9708de7125a9b73
RicardoIbarra09 <ricardo.ibarra@cinvestav.mx>
2022-10-13 15:47:34 -0500

    contrato actualizado

--- chaincode/foodcontrol/foodcontrol.go
+++ chaincode/foodcontrol/foodcontrol.go
@@ -1,81 +1,147 @@
 package main
 
-import ( 
+import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
-// SmartContract provides functions for control the food
+// SmartContract provides functions for managing a car
 type SmartContract struct {
 	contractapi.Contract
 }
 
-//Food describes basic details of what makes up a food
-type Food struct {
-	Farmer  string `json:"farmer"`
-	Variety string `json:"variety"`
+// Car describes basic details of what makes up a car
+type Car struct {
+	Make   string `json:"make"`
+	Model  string `json:"model"`
+	Colour string `json:"colour"`
+	Owner  string `json:"owner"`
 }
 
 // QueryResult structure used for handling result of query
 type QueryResult struct {
 	Key    string `json:"Key"`
-	Record *Food
-}
-func (s *SmartContract) Set(ctx contractapi.TransactionContextInterface, foodId string, farmer string, variety string) error {
-
-	//Validaciones de sintaxis
-
-	//validaciones de negocio
-
-	food := Food{
-		Farmer:  farmer,
-		Variety: variety,
-	}
-
-	foodAsBytes, err := json.Marshal(food)
-	if err != nil {
-		fmt.Printf("Marshal error: %s", err.Error())
-		return err
-	}
-
-	return ctx.GetStub().PutState(foodId, foodAsBytes)
+	Record *Car
 }
 
-func (s *SmartContract) Query(ctx contractapi.TransactionContextInterface, foodId string) (*Food, error) {
+// InitLedger adds a base set of cars to the ledger
+func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+	cars := []Car{
+		Car{Make: "Toyota", Model: "Prius", Colour: "blue", Owner: "Tomoko"},
+		Car{Make: "Ford", Model: "Mustang", Colour: "red", Owner: "Brad"},
+		Car{Make: "Hyundai", Model: "Tucson", Colour: "green", Owner: "Jin Soo"},
+		Car{Make: "Volkswagen", Model: "Passat", Colour: "yellow", Owner: "Max"},
+		Car{Make: "Tesla", Model: "S", Colour: "black", Owner: "Adriana"},
+		Car{Make: "Peugeot", Model: "205", Colour: "purple", Owner: "Michel"},
+		Car{Make: "Chery", Model: "S22L", Colour: "white", Owner: "Aarav"},
+		Car{Make: "Fiat", Model: "Punto", Colour: "violet", Owner: "Pari"},
+		Car{Make: "Tata", Model: "Nano", Colour: "indigo", Owner: "Valeria"},
+		Car{Make: "Holden", Model: "Barina", Colour: "brown", Owner: "Shotaro"},
+	}
 
-	foodAsBytes, err := ctx.GetStub().GetState(foodId)
+	for i, car := range cars {
+		carAsBytes, _ := json.Marshal(car)
+		err := ctx.GetStub().PutState("CAR"+strconv.Itoa(i), carAsBytes)
+
+		if err != nil {
+			return fmt.Errorf("Failed to put to world state. %s", err.Error())
+		}
+	}
+
+	return nil
+}
+
+// CreateCar adds a new car to the world state with given details
+func (s *SmartContract) CreateCar(ctx contractapi.TransactionContextInterface, carNumber string, make string, model string, colour string, owner string) error {
+	car := Car{
+		Make:   make,
+		Model:  model,
+		Colour: colour,
+		Owner:  owner,
+	}
+
+	carAsBytes, _ := json.Marshal(car)
+
+	return ctx.GetStub().PutState(carNumber, carAsBytes)
+}
+
+// QueryCar returns the car stored in the world state with given id
+func (s *SmartContract) QueryCar(ctx contractapi.TransactionContextInterface, carNumber string) (*Car, error) {
+	carAsBytes, err := ctx.GetStub().GetState(carNumber)
 
 	if err != nil {
 		return nil, fmt.Errorf("Failed to read from world state. %s", err.Error())
 	}
 
-	if foodAsBytes == nil {
-		return nil, fmt.Errorf("%s does not exist", foodId)
+	if carAsBytes == nil {
+		return nil, fmt.Errorf("%s does not exist", carNumber)
 	}
 
-	food := new(Food)
+	car := new(Car)
+	_ = json.Unmarshal(carAsBytes, car)
 
-	err = json.Unmarshal(foodAsBytes, food)
-	if err != nil {
-		return nil, fmt.Errorf("Unmarshal error. %s", err.Error())
-	}
-
-	return food, nil
+	return car, nil
 }
 
+// QueryAllCars returns all cars found in world state
+func (s *SmartContract) QueryAllCars(ctx contractapi.TransactionContextInterface) ([]QueryResult, error) {
+	startKey := ""
+	endKey := ""
+
+	resultsIterator, err := ctx.GetStub().GetStateByRange(startKey, endKey)
+
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	results := []QueryResult{}
+
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+
+		if err != nil {
+			return nil, err
+		}
+
+		car := new(Car)
+		_ = json.Unmarshal(queryResponse.Value, car)
+
+		queryResult := QueryResult{Key: queryResponse.Key, Record: car}
+		results = append(results, queryResult)
+	}
+
+	return results, nil
+}
+
+// ChangeCarOwner updates the owner field of car with given id in world state
+func (s *SmartContract) ChangeCarOwner(ctx contractapi.TransactionContextInterface, carNumber string, newOwner string) error {
+	car, err := s.QueryCar(ctx, carNumber)
+
+	if err != nil {
+		return err
+	}
+
+	car.Owner = newOwner
+
+	carAsBytes, _ := json.Marshal(car)
+
+	return ctx.GetStub().PutState(carNumber, carAsBytes)
+}
 
 func main() {
 
 	chaincode, err := contractapi.NewChaincode(new(SmartContract))
 
 	if err != nil {
-		fmt.Printf("Error create foodcontrol chaincode: %s", err.Error())
+		fmt.Printf("Error create fabcar chaincode: %s", err.Error())
 		return
 	}
 
 	if err := chaincode.Start(); err != nil {
-		fmt.Printf("Error starting foodcontrol chaincode: %s", err.Error())
+		fmt.Printf("Error starting fabcar chaincode: %s", err.Error())
 	}
 }
